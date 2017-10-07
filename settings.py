487d56e2f9d925e5e236fa56b2475fa0bb63b36b
Priyanka <ranipriyanka713@gmail.com>
2017-10-07 15:14:57 +0530

    online database creidential added

--- settings.py
+++ settings.py
@@ -3,12 +3,12 @@ import os
 # we want to seamlessy run our API both locally and on Heroku.
 # If running on Heroku, sensible DB connection settings are
 # stored in environment variables.
-MONGO_HOST = os.environ.get('MONGO_HOST', 'localhost')
-MONGO_PORT = os.environ.get('MONGO_PORT', 27017)
+MONGO_HOST = os.environ.get('MONGO_HOST', 'ds113505.mlab.com')
+MONGO_PORT = os.environ.get('MONGO_PORT', 13505)
 MONGO_USERNAME = os.environ.get\
-    ('MONGO_USERNAME', 'admin') # FIXME set username later
+    ('MONGO_USERNAME', 'karmadbuser1') # FIXME set username later
 MONGO_PASSWORD = os.environ.get\
-    ('MONGO_PASSWORD', '123') # FIXME set password later
+    ('MONGO_PASSWORD', 'karmadbuser1') # FIXME set password later
 MONGO_DBNAME = os.environ.get\
     ('MONGO_DBNAME', 'karma')
 
