af6059e729e9cfa104a2d4c93080665415daa0b3
Priyanka <ranipriyanka713@gmail.com>
2017-10-10 17:02:47 +0530

    token genrate, bycrypt & session

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
 import os
-from flask import request, render_template
+from flask import request, render_template, session, url_for, redirect
 from db import Mdb
 import traceback
+import json
+from werkzeug.utils import secure_filename
+from flask.ext.bcrypt import Bcrypt
 from eve import Eve
+from flask_login import LoginManager, UserMixin, login_user, login_required,\
+                        logout_user, current_user
 
-#app = Flask(__name__)
-tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'templates')
+tmpl_dir = os.path.join(os.path.dirname(os.path.abspath(__file__)),
+                        'templates')
 app = Eve('Karma-App', template_folder=tmpl_dir)
-
+bcrypt = Bcrypt(app)
 mdb = Mdb()
+
+app.config['secretkey'] = 'some-strong+secret#key'
+app.secret_key = 'F12Zr47j\3yX R~X@H!jmM]Lwf/,?KT'
+
+# setup login manager
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+class JSONEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, ObjectId):
+            return str(o)
+        return json.JSONEncoder.default(self, o)
+
+
+#############################################
+#                                           #
+#                SESSION COUNTER            #
+#                                           #
+#############################################
+def sumSessionCounter():
+    try:
+        session['counter'] += 1
+    except KeyError:
+        session['counter'] = 1
+
 
 @app.route('/')
 def main_page():
@@ -18,11 +50,39 @@ def main_page():
 
 #############################################
 #                                           #
+#              TOKEN REQUIRED               #
+#                                           #
+#############################################
+app.config['secretkey'] = 'some-strong+secret#key'
+
+
+def token_required(f):
+    @wraps(f)
+    def decorated(*args, **kwargs):
+        token = request.args.get('token')
+
+        # ensure that token is specified in the request
+        if not token:
+            return jsonify({'message': 'Missing token!'})
+
+        # ensure that token is valid
+        try:
+            data = jwt.decode(token, app.config['secretkey'])
+        except:
+            return jsonify({'message': 'Invalid token!'})
+
+        return f(*args, **kwargs)
+
+    return decorated
+
+
+#############################################
+#                                           #
 #              REGISTER COMPANY             #
 #                                           #
 #############################################
 @app.route("/company/signup", methods=['POST'])
-def add_user():
+def add_company():
     try:
         name = request.form['name']
         registration = request.form['regis']
@@ -37,12 +97,16 @@ def add_user():
         password = request.form['password']
         website = request.form['website']
 
-        mdb.add_user(name, registration, city, state, zip, country, company,
-                     phone, email, password, website)
-        print('User is added successfully')
+        # password bcrypt  #
+        pw_hash = bcrypt.generate_password_hash(password)
+        passw = bcrypt.check_password_hash(pw_hash, password)
+
+        mdb.add_company(name, registration, city, state, zip, country,
+                        company, phone, email, pw_hash, website)
+        print('Company is added successfully')
         templateData = {'title': 'Signin Page'}
     except Exception as exp:
-        print('add_user() :: Got exception: %s' % exp)
+        print('add_company() :: Got exception: %s' % exp)
         print(traceback.format_exc())
     return render_template('company/signin.html', **templateData)
 
@@ -72,20 +136,54 @@ def signin():
 #############################################
 @app.route('/login', methods=['POST'])
 def login():
-
+    ret = {'err': 0}
     try:
+        sumSessionCounter()
         email = request.form['email']
         password = request.form['password']
-        if mdb.company_exists(email, password):
-            print 'Login Successfully'
-            templateData = {'title': 'singin page'}
+
+        if mdb.company_exists(email):
+            pw_hash = mdb.get_password(email)
+            print 'password in server, get from db class', pw_hash
+            passw = bcrypt.check_password_hash(pw_hash, password)
+
+            if passw == True:
+                name = mdb.get_name(email)
+                session['name'] = name
+                session['email'] = email
+
+                # Login Successful!
+                expiry = datetime.datetime.utcnow() + datetime.\
+                    timedelta(minutes=30)
+                token = jwt.encode({'user': email, 'exp': expiry},
+                                   app.config['secretkey'], algorithm='HS256')
+
+                ret['msg'] = 'Login successful'
+                ret['err'] = 0
+                ret['token'] = token.decode('UTF-8')
+                templateData = {'title': 'singin page'}
+            else:
+                return render_template('/company/save.html', session=session)
+
         else:
-            return render_template('/company/index.html', **templateData)
+            # Login Failed!
+            return render_template('/company/save.html', session=session)
+
     except Exception as exp:
-        print 'login() :: Got exception: %s' % exp
+        ret['msg'] = '%s' % exp
+        ret['err'] = 1
         print(traceback.format_exc())
-    templateData = {'title': 'singin page'}
-    return render_template('/company/save.html', **templateData)
+    # return jsonify(ret)
+    return render_template('/company/save.html', session=session)
+
+
+#############################################
+#              SESSION LOGOUT               #
+#############################################
+@app.route('/clear')
+def clearsession():
+    session.clear()
+    return render_template('/company/index.html', session=session)
 
 
 #############################################
@@ -94,4 +192,5 @@ def login():
 #                                           #
 #############################################
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
